a28781bb6ae7de2db5e9558d94f415c876722cb0
Nicolás Venturo <nicolas.venturo@gmail.com>
2026-05-07 19:28:07 +0000

    Add option to hide spell flavor text on print page.
    Adds a "Hide spell flavor text" checkbox on the Print view that strips
    italicized lore lines (matching /^\*[^*]+\*$/) from spell entries before
    rendering. Also splits the Spells printable button out of the magic gate
    so it only appears for armies with actual spells (excludes Dwarfs and
    Nippon, which use magic items but have no Wizards).

--- src/views/Print.vue
+++ src/views/Print.vue
@@ -4,9 +4,13 @@
        <button v-for="(item, index) in printableItems" :key="index" @click="addPrintItem(index)">{{item.title}}</button>
     </div>
 
+    <label v-if="hasSpells" class="print-options">
+      <input type="checkbox" v-model="noFluff"> Hide spell flavor text
+    </label>
+
     <p>Click what you would like to print in the box above to add it to the preview below, then <a href="javascript:window.print()">print</a>.</p>
 
-    <PrintItems />
+    <PrintItems :noFluff="noFluff" />
   </main>
 </template>
 
@@ -17,8 +21,10 @@ import store from '@/store';
 export default {
   name: 'PrintView',
   components: { PrintItems },
+  data: () => ({ noFluff: false }),
   computed: {
-    printableItems: () => store.getters.printableItems
+    printableItems: () => store.getters.printableItems,
+    hasSpells: () => Array.isArray(store.getters.spells) && store.getters.spells.length > 0
   },
   methods: {
     addPrintItem (index) {
@@ -50,6 +56,17 @@ export default {
       font-size: 1.2rem;
       line-height: $_ - .2rem;
       margin: ($_ / 4) .5em ($_ / 4) 0;
+    }
+
+    .print-options {
+      display: block;
+      margin: 0 0 ($_ / 2);
+    }
+
+    @media print {
+      .print-options {
+        display: none;
+      }
     }
   }
 </style>
